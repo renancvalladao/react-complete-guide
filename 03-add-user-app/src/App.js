cc7b9a95be4bcfabc56514d1ad651030e86aacd7
Renan Valladão <renancvalladao@gmail.com>
2023-02-21 16:19:04 -0300

    Managing list of users

--- 03-add-user-app/src/App.js
+++ 03-add-user-app/src/App.js
@@ -1,11 +1,21 @@
+import { useState } from 'react'
 import AddUser from './components/Users/AddUser'
 import UsersList from './components/Users/UsersList'
 
 function App() {
+  const [usersList, setUsersList] = useState([])
+
+  const addUserHandler = (name, age) => {
+    setUsersList((prevUsersList) => [
+      ...prevUsersList,
+      { id: Math.random().toString(), name, age }
+    ])
+  }
+
   return (
     <div>
-      <AddUser />
-      <UsersList users={[]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   )
 }
