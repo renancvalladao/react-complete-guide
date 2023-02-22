4b6792b1d11d875b8b79810d876ee39baff87608
Renan Valladão <renancvalladao@gmail.com>
2023-02-22 14:27:29 -0300

    Adding portal example

--- 03-add-user-app/src/components/UI/ErrorModal.js
+++ 03-add-user-app/src/components/UI/ErrorModal.js
@@ -1,23 +1,44 @@
+import ReactDOM from 'react-dom'
 import Button from './Button'
 import Card from './Card'
 import classes from './ErrorModal.module.css'
 
+const BackDrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose} />
+}
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onClose}>Okay</Button>
+      </footer>
+    </Card>
+  )
+}
+
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onClose} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onClose}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <BackDrop onClose={props.onClose} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onClose={props.onClose}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </>
   )
 }
 
