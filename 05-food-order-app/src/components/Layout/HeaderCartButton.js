42ae61f1b6b5b93446e47a31f01065cc62d114ef
Renan Valladão <renancvalladao@gmail.com>
2023-03-01 18:14:46 -0300

    Using useEffect to animate button on change cart

--- 05-food-order-app/src/components/Layout/HeaderCartButton.js
+++ 05-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,10 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import CartContext from '../../store/cart-context'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
   const cartCtx = useContext(CartContext)
 
   const numberOfCartItems = cartCtx.items.reduce(
@@ -11,8 +12,19 @@ const HeaderCartButton = (props) => {
     0
   )
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted && classes.bump}`
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) return
+    setBtnIsHighlighted(true)
+
+    const timer = setTimeout(() => setBtnIsHighlighted(false), 300)
+
+    return () => clearTimeout(timer)
+  }, [cartCtx.items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
